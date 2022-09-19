f6b3c1dc3ee1ffaff560b21251423d9bbeb31fa9
Philipp Wuerfel <phi.wuerfel@gmail.com>
2022-09-19 13:41:38 +0200

    update consumers.py to use asyncio

--- services/dj_queues/polls/consumers.py
+++ services/dj_queues/polls/consumers.py
@@ -2,7 +2,7 @@ import json
 
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 from celery.result import AsyncResult
 
 
@@ -41,20 +41,39 @@ def notify_channel_layer(task_id):
     )
 
 
-class TaskStatusConsumer(WebsocketConsumer):
-    def connect(self):
+# class TaskStatusConsumer(WebsocketConsumer):
+#     def connect(self):
+#         self.task_id = self.scope["url_route"]["kwargs"]["task_id"]
+
+#         async_to_sync(self.channel_layer.group_add)(self.task_id, self.channel_name)
+
+#         self.accept()
+
+#         self.send(text_data=json.dumps(get_task_info(self.task_id)))
+
+#     def disconnect(self, close_code):
+#         async_to_sync(self.channel_layer.group_discard)(self.task_id, self.channel_name)
+
+#     def update_task_status(self, event):
+#         data = event["data"]
+
+#         self.send(text_data=json.dumps(data))
+
+
+class TaskStatusConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.task_id = self.scope["url_route"]["kwargs"]["task_id"]
 
-        async_to_sync(self.channel_layer.group_add)(self.task_id, self.channel_name)
+        await self.channel_layer.group_add(self.task_id, self.channel_name)
 
-        self.accept()
+        await self.accept()
 
-        self.send(text_data=json.dumps(get_task_info(self.task_id)))
+        await self.send(text_data=json.dumps(get_task_info(self.task_id)))
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(self.task_id, self.channel_name)
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(self.task_id, self.channel_name)
 
-    def update_task_status(self, event):
+    async def update_task_status(self, event):
         data = event["data"]
 
-        self.send(text_data=json.dumps(data))
+        await self.send(text_data=json.dumps(data))
